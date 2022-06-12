61989113590f62b96134fd673cfba7e36d794443
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-12 17:10:51 -0300

    fix: broken btn submit

--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,32 +1,19 @@
 import './InputForm.sass';
+import $ from 'jquery';
 
 import React from 'react';
 
 let errorMessage = "Can't be empty";
 
 const InputForm = (props) => {
-  let isEmpty = false;
-
-  const handleEmpty = (e) => {
-    let value = e.target.value;
-    if (value === '') {
-      isEmpty = true;
-    } else {
-      isEmpty = false;
-    }
-  }
-
-
 
   const { type, title } = props;
   const handleType = (type) => { type = type ? type : "text"; return type }
   const handleTitle = (title) => { title = title ? title : "Insira um Título"; return title }
 
   return (
-    <div className={'InputForm'} id="inputForm">
-      <input required type={handleType(type)} autoComplete={type} placeholder={handleTitle(title)} onChange={(e) => {
-        handleEmpty(e);
-      }} />
+    <div className='InputForm'>
+      <input required type={handleType(type)} autoComplete={type} placeholder={handleTitle(title)} />
       <span className='hidden' id="error_message">{errorMessage}</span>
     </div >
   );
