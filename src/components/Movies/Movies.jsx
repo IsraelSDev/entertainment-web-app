a0c8f8e04f82895bfff0863eac5f1a34a97e5010
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-13 20:12:25 -0300

    new: finish layout before logic implementation

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import Card from '../../components/Card/Card';
+import "./Movies.sass";
 
 const Movies = () => {
-  return (
+  return (<>
     <div>
       <h1>Movies</h1>
     </div>
-  );
+    <div className='movies-container'>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+    </div>
+  </>);
 }
 
 export default Movies;
--- src/components/Series/Series.jsx
+++ src/components/Series/Series.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import Card from '../../components/Card/Card';
 
 const Series = () => {
-  return (
+  return (<>
     <div>
       <h1>Series</h1>
     </div>
-  );
+    <div className='movies-container'>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+      <div className='movies-wrapper'>
+        <Card />
+      </div>
+    </div>
+  </>);
 }
 
 export default Series;
