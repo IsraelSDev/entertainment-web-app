c8375413d91bd7e2e1d311bc3c5e6bcec0d56110
Israel S. Santos <israel.santos@zukk.com.br>
2022-06-17 17:32:19 -0300

    new: search for movies implemented

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,48 @@
 import "./Search.sass";
 
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import searchIcon from '../../assets/search.svg';
 import Input from '@mui/material/Input';
 
-const ariaLabel = { 'aria-label': 'description' };
+import { sectionContext } from "../../context/sectionContext";
+import { favoriteContext } from "../../context/favoriteContext";
+import { moviesContext } from '../../context/moviesContext';
+
+
 
 const Search = () => {
+
+  const { movies, setMovies } = useContext(moviesContext);
+  const { section } = useContext(sectionContext);
+  const { favoriteMovie, handlefavoriteMovie } = useContext(favoriteContext);
+
+  const [unFilteredMovies, setUnFilteredMovies] = useState([...movies]);
+  const [unFilteredFavoriteMovies, setUnFilteredFavoriteMovies] = useState([...favoriteMovie]);
+
+  const handleSearch = (e) => {
+    const search = e.target.value;
+
+    if (section === 'Bookmarked') {
+      console.log('Bookmarked');
+      const filteredMovies = unFilteredFavoriteMovies.filter(item => item.title.toString().toLowerCase().includes(search.toLowerCase()));
+      search === '' ? handlefavoriteMovie(unFilteredFavoriteMovies) : handlefavoriteMovie(filteredMovies);
+    } else {
+      console.log('Not Bookmarked');
+      const filteredMovies = unFilteredMovies.filter(item => item.title.toString().toLowerCase().includes(search.toLowerCase()));
+      search === '' ? setMovies(unFilteredMovies) : setMovies(filteredMovies);
+    }
+
+  }
+
+
   return (
     <div className='Search'>
       <img src={searchIcon} alt='search-icon' />
-      <Input placeholder="Search for movies or TV series" inputProps={ariaLabel} color="secondary" />
+      <Input onChange={
+        (e) => {
+          handleSearch(e)
+        }
+      } placeholder="Search for movies or TV series" color="secondary" />
     </div>
   );
 }
